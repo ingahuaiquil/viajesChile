cd3bf7634d99dfc4442fbeb07b4a227864afc9cd
ingahuaiquil <ing.ahuaiquil@gmail.com>
2024-04-23 13:59:06 -0500

    fin proyecto

---
+++ assets/js/index.js
@@ -0,0 +1,7 @@
+// botón enviar correo
+var tooltipTriggerList = [].slice.call(document.querySelectorA11('[data-bs-toggle="modal"]'))
+var tooltipList = tooltipTriggerList.map(function (_tooltipTriggerE1) {
+return new bootstrap.Tooltip(tooltipTriggerEl)
+})
+
+//
